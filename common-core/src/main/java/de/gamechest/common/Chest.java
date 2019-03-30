969f93fb6a4d28554e8d88f172ba7e12cc708512
ByteList <niklas.bytelist@gmail.com>
2019-03-30 12:07:45 +0100

    Implemented cloud permission check

--- common-core/src/main/java/de/gamechest/common/Chest.java
+++ common-core/src/main/java/de/gamechest/common/Chest.java
@@ -2,6 +2,8 @@ package de.gamechest.common;
 
 import lombok.Getter;
 
+import java.util.HashMap;
+
 /**
  * Created by ByteList on 06.02.2019.
  * <p>
@@ -11,6 +13,16 @@ public class Chest {
 
     @Getter
     private static ChestPlugin instance;
+    @Getter
+    private static final HashMap<String, Rank> permissionRank = new HashMap<String, Rank>() {
+        {
+            put("command.stop", Rank.MODERATOR);
+
+            put("command.goto", Rank.BUILDER);
+            put("command.join", Rank.BUILDER);
+            put("command.server", Rank.SUPPORTER);
+        }
+    };
 
     public static void setInstance(ChestPlugin plugin) {
         if(instance != null) {
